bcb00456cad7331acdeed709f9a9243848b477b6
curiositypaths <curiositypaths@users.noreply.github.com>
2018-11-09 14:49:21 -0500

    Async with React sample

--- src/App.js
+++ src/App.js
@@ -1,28 +1,137 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+const apiAddress =
+  "http://fetch-message-in-the-bottle.herokuapp.com/api/v2/messages";
+
+class Messages extends React.Component {
+  state = {
+    messages: [],
+    newMessageBody: "",
+    newMessageAuthor: "",
+    action: "Post",
+    currentMsg: null
+  };
+
+  componentDidMount() {
+    fetch(apiAddress)
+      .then(r => r.json())
+      .then(messages => {
+        this.setState({ messages });
+      });
+  }
+
+  handleInputsUpdates = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleMsgEdit = msgId => {
+    const msgToEdit = this.state.messages.find(m => m.id === msgId);
+    this.setState({
+      newMessageBody: msgToEdit.message,
+      newMessageAuthor: msgToEdit.real_name,
+      action: "Edit",
+      currentMsg: msgToEdit.id
+    });
+    console.log(msgToEdit);
+  };
+
+  handlePostAndEdit = (verb, path) => {
+    const message = this.state.newMessageBody;
+    const real_name = this.state.newMessageAuthor;
+    //const { newMessageBody: message, newMessageAuthor: real_name } = this.state;
+    const body = { message: { message, real_name } };
+    const config = {
+      method: verb,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    };
+    fetch(path, config)
+      .then(r => r.json())
+      .then(msgObj => {
+        let messages;
+        if (this.state.action === "Post") {
+          messages = [msgObj, ...this.state.messages];
+        }
+        if (this.state.action === "Edit") {
+          messages = [
+            msgObj,
+            ...this.state.messages.filter(m => m.id !== this.state.currentMsg)
+          ];
+        }
+        // we will have a variable messages with the updated state
+        this.setState({
+          messages,
+          newMessageBody: "",
+          newMessageAuthor: "",
+          currentMsg: null
+        });
+      });
+  };
+
+  handleTweetPost = () => {
+    if (this.state.action === "Post") {
+      this.handlePostAndEdit("POST", apiAddress);
+    }
+    if (this.state.action === "Edit") {
+      this.handlePostAndEdit("PATCH", `${apiAddress}/${this.state.currentMsg}`);
+    }
+  };
+
+  handleDelete = (msgId, event) => {
+    event.stopPropagation();
+    // this.setState({
+    //   messages: this.state.messages.filter(m => m.id !== msgId)
+    // });
+    fetch(`${apiAddress}/${msgId}`, { method: "DELETE" })
+      .then(r => r.json())
+      .then(
+        c => {
+          this.setState({
+            messages: this.state.messages.filter(m => m.id !== msgId)
+          });
+        },
+        () => {
+          alert("something went wrong");
+        }
+      );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <>
+        Message:
+        <input
+          name="newMessageBody"
+          type="text"
+          onChange={this.handleInputsUpdates}
+          value={this.state.newMessageBody}
+        />
+        <br />
+        Author:{" "}
+        <input
+          name="newMessageAuthor"
+          type="text"
+          onChange={this.handleInputsUpdates}
+          value={this.state.newMessageAuthor}
+        />
+        <br />
+        <button onClick={this.handleTweetPost}>{this.state.action}</button>{" "}
+        <br />
+        {this.state.messages.map(mObj => (
+          <p onClick={() => this.handleMsgEdit(mObj.id)} key={mObj.id}>
+            {mObj.message}{" "}
+            <button onClick={event => this.handleDelete(mObj.id, event)}>
+              delete
+            </button>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        ))}
+      </>
     );
   }
 }
 
-export default App;
+export default Messages;
